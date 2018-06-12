2a76c6fe7f4e332fd005a0dd245a63c3ac2524b4
Sven <s_devries@live.nl>
2018-06-12 23:49:32 +0200

    sdfs

--- Database/start-up.sh
+++ Database/start-up.sh
@@ -5,3 +5,11 @@ mysql -u root < /honepot-project/Database/info.sql
 
 # change config file for logging
 cp -f /honepot-project/Database/50-server.cnf /etc/mysql/mariadb.conf.d/
+
+# restart service
+service mysql restart
+
+# change general logging to ON
+mysql -u root -e 'SET GLOBAL general_log = 'ON';'
+
+ls /var/log/mysql/
